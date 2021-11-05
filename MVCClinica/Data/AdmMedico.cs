2696e77a15b414d249b2fa53b6b45447cf5e6581
franarci <fran.arciniega96@gmail.com>
2021-11-05 20:57:10 -0300

    AdmMedico con Listar, Insertar, GetPorId

---
+++ MVCClinica/Data/AdmMedico.cs
@@ -0,0 +1,29 @@
+﻿using MVCClinica.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCClinica.Data
+{
+    public static class AdmMedico
+    {
+        private static MedicoDbContext context = new MedicoDbContext();
+
+        public static List<Medico> Listar()  //Modelo conectado
+        {
+            return context.Medicos.ToList();           
+        }
+
+        internal static void Insertar(Medico medico)
+        {
+            context.Medicos.Add(medico);
+            context.SaveChanges();
+        }
+
+        internal static Medico GetPorId(int id)
+        {
+            return context.Medicos.Find(id);
+        }
+    }
+}
